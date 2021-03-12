bdafedfad2ce7de6ae6c0be8826fb10ef05e5227
hewerton <hewerton80@gmail.com>
2021-03-12 14:08:41 -0300

    changes

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+const colors = {
+    header: '#283e4a',
+    linkedin: '#0077b5',
+    input: '#e1e9ee',
+    icons: '#c7d1d8',
+    hashtag: '#7a8b98',
+    background: '#f5f5f5',
+    adText: '#434649',
+    panel: '#fff',
+    link: '#0073b1',
+    black: 'rgba(0,0,0,.9)',
+    gray: 'rgba(0,0,0,.6)',
+    separator: 'rgba(0,0,0,0.15)',
+    white: '#fff'
+}
+
+export default colors;
